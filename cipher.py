4d40ad587b8ec31bcbce3b70a384403cb431c37e
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-08-26 22:16:10 +0200

    Update cipher.py
    Added options for users

--- cipher.py
+++ cipher.py
@@ -1,7 +1,23 @@
-input = 'Working'
-encrypted = ''
+# the user has options to encrypt or decrypt a message or exit the program
+print('Welcome to the Caesar Cipher Program')
+print('1. Encrypt')
+print('2. Decrypt')     
+print('3. Exit')
 
-for x in input:
-   encrypted = encrypted + chr(ord(x) +1)
+option= input('Choose an Option(1,2,3): ')
 
-print(encrypted)
+if option == '1':
+   plaintext = input('Enter the message to encrypt: ')
+   encrypted = ''
+   for x in plaintext:
+      if x.isalpha():
+         encrypted = encrypted + chr(ord(x) +1)
+   print(encrypted)
+else:
+ if option == '2':
+      encryptedmessage = input('Enter the message to decrypt: ')
+      decrypted = ''
+      for x in encryptedmessage:
+         if x.isalpha():
+            decrypted = decrypted + chr(ord(x) -1)
+      print(decrypted)
