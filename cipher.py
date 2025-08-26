626e0da43142552478cfbc1bcf23b44233350dd9
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-08-26 23:05:03 +0200

    Update cipher.py
    Added a while loop that makes use of a boolean, the end goal is really to determine what the user enters. If not entered within that range(1,2) the loop will repeat, giving the users the options again either to encrypt another message or just decrypt it or exit the program

--- cipher.py
+++ cipher.py
@@ -1,23 +1,36 @@
 # the user has options to encrypt or decrypt a message or exit the program
 print('Welcome to the Caesar Cipher Program')
-print('1. Encrypt')
-print('2. Decrypt')     
-print('3. Exit')
 
-option= input('Choose an Option(1,2,3): ')
 
-if option == '1':
-   plaintext = input('Enter the message to encrypt: ')
-   encrypted = ''
-   for x in plaintext:
-      if x.isalpha():
-         encrypted = encrypted + chr(ord(x) +1)
-   print(encrypted)
-else:
- if option == '2':
+validnumber = False
+
+while validnumber == False:
+
+   print('1. Encrypt')
+   print('2. Decrypt')     
+   print('3. Exit')
+   option= input('Choose an Option(1,2,3): ')  
+   if option == '1':
+      plaintext = input('Enter the message to encrypt: ')
+      encrypted = ''
+      for x in plaintext:
+         if x.isalpha():
+          encrypted = encrypted + chr(ord(x) +1)
+         else:
+          encrypted = encrypted + x
+      print('Encrypted Message: ' + encrypted)
+   elif option == '2':
       encryptedmessage = input('Enter the message to decrypt: ')
       decrypted = ''
       for x in encryptedmessage:
          if x.isalpha():
             decrypted = decrypted + chr(ord(x) -1)
-      print(decrypted)
+         else:
+            decrypted = decrypted + x
+      print('Decrypted Message: ' + decrypted)
+   elif option == '3':
+      validnumber = True
+
+
+print('Bye!')
+exit()
