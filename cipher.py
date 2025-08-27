120a375f6791dfcf5b3d25d2e80e6c2992c967da
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-08-27 03:21:00 +0200

    Update cipher.py
    Allowed the user to enter the shift key they want to use to encrypt their message and I also improved the Command Line UI a bit so the options can be clear for the user to choose from.

--- cipher.py
+++ cipher.py
@@ -1,30 +1,32 @@
 # the user has options to encrypt or decrypt a message or exit the program
-print('Welcome to the Caesar Cipher Program')
-
-
 validnumber = False
 
 while validnumber == False:
-
+   print('Welcome to the Caesar Cipher Program')
+   print('============================')
+   print('Options:')
    print('1. Encrypt')
    print('2. Decrypt')     
    print('3. Exit')
+   print('============================') #just so the options are clear
    option= input('Choose an Option(1,2,3): ')  
    if option == '1':
       plaintext = input('Enter the message to encrypt: ')
+      encryptkey = input('Enter the encryption shift key: ')
       encrypted = ''
       for x in plaintext:
          if x.isalpha():
-          encrypted = encrypted + chr(ord(x) +1)
+          encrypted = encrypted + chr(ord(x) +int(encryptkey))
          else:
           encrypted = encrypted + x
       print('Encrypted Message: ' + encrypted)
    elif option == '2':
       encryptedmessage = input('Enter the message to decrypt: ')
       decrypted = ''
+      decryptkey = input('Enter the decryption shift key: ')
       for x in encryptedmessage:
          if x.isalpha():
-            decrypted = decrypted + chr(ord(x) -1)
+            decrypted = decrypted + chr(ord(x) - int(decryptkey))
          else:
             decrypted = decrypted + x
       print('Decrypted Message: ' + decrypted)
@@ -32,5 +34,5 @@ while validnumber == False:
       validnumber = True
 
 
-print('Bye!')
+print('Have a great day, Bye!')
 exit()
